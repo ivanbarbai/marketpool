28dba38111121557928686ae0dec44fe76f9a866
Ivan <cruzivanbl@gmail.com>
2022-07-22 01:22:19 -0500

    Modifying functions based on approach; Manage the partners as a pool

---
+++ src/components/Partners/PartnerAction.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PartnerForm from './PartnerForm'
+
+const PartnerAction = () => {
+  return (
+    <>
+      <PartnerForm />
+    </>
+  )
+}
+
+export default PartnerAction
---
+++ src/components/Partners/PartnerForm.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PartnerForm = () => {
+  return (
+    <form className='card-full'>
+      <div className="card-header">Nuevo socio</div>
+      <div className="card-content">
+
+      </div>
+    </form>
+  )
+}
+
+export default PartnerForm
